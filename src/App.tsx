f7dc58f799da21e24e40ba5d5e4405ee29e9b901
Su <skibasu@gmail.com>
2023-09-08 01:29:47 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Button } from "@material-tailwind/react"
+import "./App.css"
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline text-red-600">
+                Simple React Typescript Tailwind Sample
+            </h1>
+            <Button>Button</Button>
+        </div>
+    )
+}
+
+export default App
