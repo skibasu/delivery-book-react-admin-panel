cb45ad2cd836c63ec09c48a25d31413f916ae41f
su <lukasz.skiba@frontdoor.com>
2023-12-11 01:01:11 +0100

    DEL-42:id-set-properly

--- src/features/auth/initialState.ts
+++ src/features/auth/initialState.ts
@@ -1,7 +1,7 @@
 import { AuthState } from "./types"
 console.log(document)
 export const initialState: AuthState = {
-    _id: null,
+    _id: document.cookie.replace("_id=", ""),
     token: document.cookie.replace("_id=", ""),
     loading: "idle",
     error: null,
--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,21 +1,18 @@
 import { useAppSelector } from "@/hooks/useStore"
-import React, { useEffect } from "react"
+import React from "react"
 import AuthForm from "../../components/AuthForm/AuthForm"
-import { useNavigate } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 
-const Home: React.FC = () => {
-    const { token } = useAppSelector((state) => state.auth)
-    const navigate = useNavigate()
+const Login: React.FC = () => {
+    const { _id } = useAppSelector((state) => state.auth)
 
-    useEffect(() => {
-        token && navigate("/", { replace: true })
-        //eslint-disable-next-line
-    }, [token])
-    return (
+    return !_id ? (
         <section className="grow flex flex-column justify-center items-center bg-storm">
             <AuthForm />
         </section>
+    ) : (
+        <Navigate to="/" replace={true} />
     )
 }
 
-export default Home
+export default Login
