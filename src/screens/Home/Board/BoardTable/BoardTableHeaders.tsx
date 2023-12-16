468868c0cb02d3412f66e5c332fc10c055601a4b
su <lukasz.skiba@frontdoor.com>
2023-12-16 02:06:24 +0100

    DEL-42:working on sorting table

--- src/screens/Home/Board/BoardTable/BoardTableHeaders.tsx
+++ src/screens/Home/Board/BoardTable/BoardTableHeaders.tsx
@@ -4,11 +4,25 @@ import { tableHeaders } from "../labels/headers"
 import { ReactComponent as SortIcon } from "@/assets/svg/icon-sort.svg"
 import { useDispatch } from "react-redux"
 import { sortOrderByKey } from "@/features/orders/ordersSlice"
+
 interface IBoardTableHeaders {
     boardType: OrderStatus
 }
+
+interface ISortState {
+    createdAt: boolean
+    price: boolean
+    paymentType: boolean
+    selectedBy: boolean
+}
+const initialSortState = {
+    createdAt: false,
+    price: false,
+    paymentType: false,
+    selectedBy: false,
+}
 const BoardTableHeaders: React.FC<IBoardTableHeaders> = ({ boardType }) => {
-    const [isASC, setIsASC] = useState<boolean>(false)
+    const [isASC, setIsASC] = useState<ISortState>(initialSortState)
     const dispatch = useDispatch()
     return (
         <div className="w-full flex bg-customGrayLight rounded-sm mt-8x mb-7x [&>*:first-child]:border-l-0 shrink-0">
@@ -30,15 +44,21 @@ const BoardTableHeaders: React.FC<IBoardTableHeaders> = ({ boardType }) => {
                         <span>{label}</span>
                         {sort ? (
                             <span
-                                className="block w-[10px] h-[10px]"
+                                className={`block w-[10px] h-[10px] rotate-${
+                                    isASC[key as keyof ISortState] ? "180" : "0"
+                                }`}
                                 onClick={() => {
                                     console.log("sort")
-                                    setIsASC(!isASC)
+                                    setIsASC((state: any) => ({
+                                        ...initialSortState,
+                                        [key as keyof ISortState]:
+                                            !isASC[key as keyof ISortState],
+                                    }))
                                     dispatch(
                                         sortOrderByKey({
                                             boardType,
                                             key,
-                                            asc: isASC,
+                                            asc: isASC[key as keyof ISortState],
                                         })
                                     )
                                 }}
